e6cd6d18d0579e444d8b8f0ad449d197a227af6e
sml <smlrodrigues.dev@gmail.com>
2023-02-07 11:17:03 -0300

    Add all cards

---
+++ src/utils/allCards.js
@@ -0,0 +1,55 @@
+import amaneImg from '../assets/images/amane.png';
+import daruImg from '../assets/images/daru.png';
+import farisImg from '../assets/images/faris.png';
+import kagariImg from '../assets/images/kagari.png';
+import kurisuImg from '../assets/images/kurisu.png';
+import lukaImg from '../assets/images/luka.png';
+import mayuriImg from '../assets/images/mayuri.png';
+import moekaImg from '../assets/images/moeka.png';
+import hitaroImg from '../assets/images/rintaro.png';
+import suzuhaImg from '../assets/images/suzuha.png';
+
+const allCards = [
+  {
+    name: 'Rintaro Okabe',
+    image: hitaroImg,
+  },
+  {
+    name: 'Kurisu Makise',
+    image: kurisuImg,
+  },
+  {
+    name: 'Mayuri Shiina',
+    image: mayuriImg,
+  },
+  {
+    name: 'Itaru Hashida',
+    image: daruImg,
+  },
+  {
+    name: 'Faris NyanNyan',
+    image: farisImg,
+  },
+  {
+    name: 'Luka Urushibara',
+    image: lukaImg,
+  },
+  {
+    name: 'Suzuha Amane',
+    image: suzuhaImg,
+  },
+  {
+    name: 'Yuki Amane',
+    image: amaneImg,
+  },
+  {
+    name: 'Moeka Kiryū',
+    image: moekaImg,
+  },
+  {
+    name: 'Kagari Shiina',
+    image: kagariImg,
+  },
+];
+
+export default allCards;
