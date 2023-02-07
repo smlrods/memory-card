399952362d9321ae00b78fabf5a545ef5f0a3edd
sml <smlrodrigues.dev@gmail.com>
2023-02-07 11:18:02 -0300

    Add game logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import Cards from './components/Cards';
+import Header from './components/Header';
+import allCards from './utils/allCards';
+import shuffle from './utils/shuffle';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [cards, setCards] = useState(allCards);
+  const [cardsSelected, setCardsSelected] = useState([]);
+
+  const alreadySelected = (card) => (cardsSelected.includes(card)); 
+
+  const setGameScore = (card) => {
+    if (alreadySelected(card)) {
+      setScore(0);
+      setCardsSelected([]);
+    } else {
+      setScore(score + 1);
+      setCardsSelected([...cardsSelected, card]);
+    }
+    setCards(shuffle(cards));
+  }
+
   return (
     <div className="App">
+      <Header score={score}/>
+      <Cards setGameScore={setGameScore} cards={cards}/>
     </div>
   );
 }
